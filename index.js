db44d7a943e7564b92b40a31a2188068cd704b30
Lucas Lamar <lucass.lamar@gmail.com>
2019-10-15 19:04:10 -0300

    criação de rotas do desafio

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+//teste 2
+ 
+
+const projects = [];
+
+// Rota para chamar todos usuarios
+
+server.get('/projects', (req, res) => {
+    return res.json(projects);
+  });
+  
+  //Rota para cadastrar usuarios
+  server.post('/projects', (req, res) => {
+    const { id, title } = req.body;
+  
+    const project = {
+      id,
+      title,
+      tasks: []
+    };
+  
+    projects.push(project);
+  
+    return res.json(project);
+  });
+
+  // rota para editar usuarios
+  
+  server.put('/projects/:id',  (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+  
+    const project = projects.find(p => p.id == id);
+  
+    project.title = title;
+  
+    return res.json(project);
+  });
+  
+  server.delete('/projects/:id',  (req, res) => {
+    const { id } = req.params;
+  
+    const projectIndex = projects.findIndex(p => p.id == id);
+  
+    projects.splice(projectIndex, 1);
+  
+    return res.send();
+  });
+  
+ 
+  server.post('/projects/:id/tasks',  (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+  
+    const project = projects.find(p => p.id == id);
+  
+    project.tasks.push(title);
+  
+    return res.json(project);
+  });
+
+server.listen(3002);
